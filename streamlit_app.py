7dee04b0d99e8d0db8d9ccba8089daeb9f5dada6
Turyahabwa Mark <83297358+MarkTLite@users.noreply.github.com>
2022-09-19 14:01:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,14 @@
 """
 Created on Wed Sep 14 10:28:48 2022
 
-@author: kalul
+@Author: Kalulu Waako 
 """
-
 from random import randint
 import numpy as np
 import pickle
 import streamlit as st
 
 # Creating a function for prediction
-
 def DDoS_Prediction(input_data):
 
     # loading the saved model
@@ -21,7 +19,6 @@ def DDoS_Prediction(input_data):
 
     # reshape the array as we are predicting for one instance
     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-
 
     prediction = loaded_model.predict(input_data_reshaped)
     print(prediction)
@@ -34,43 +31,40 @@ def DDoS_Prediction(input_data):
 def main():
     #Giving a title
     st.title('DDoS Attack Prediction Web App')
+    col1, col2, col3 = st.columns(3)
     
     # getting the input data from the user
-    pktcount = st.text_input('Packet Count')
-    bytecount = st.text_input('Byte Count')
-    dur = st.text_input('Duration')
-    dur_nsec = st.text_input('Duration of n_second')
-    tot_dur = st.text_input('Total Duration')
-    flows = st.text_input('Total flows')
-    pktperflow = st.text_input('Packet per flow')
-    byteperflow = st.text_input('Byte per flow')
-    pktrate = st.text_input('Packet Rate')
-    port_no = st.text_input('Port number')
-    tx_bytes = st.text_input('Transmitted bytes')
-    rx_bytes = st.text_input('Received bytes')
-    tx_kbps = st.text_input('Transmitted kilobytes')
+    with col1:
+      pktcount = st.text_input('Packet Count')
+      bytecount = st.text_input('Byte Count')
+      dur = st.text_input('Duration')
+      dur_nsec = st.text_input('Duration of n_second')
+
+    with col2:
+      tot_dur = st.text_input('Total Duration')
+      flows = st.text_input('Total flows')
+      pktperflow = st.text_input('Packet per flow')
+      byteperflow = st.text_input('Byte per flow')
+
+    with col3:
+      pktrate = st.text_input('Packet Rate')
+      port_no = st.text_input('Port number')
+      tx_bytes = st.text_input('Transmitted bytes')
+      rx_bytes = st.text_input('Received bytes')
+      tx_kbps = st.text_input('Transmitted kilobytes')
     
     #code for prediction
     network = ''
     
     # creating a button for Prediction
-
-   
-    
     if st.button('DDoS Test Result'):
         network = DDoS_Prediction(
-          [float(pktcount),
-           float(bytecount),
-            float(dur), float(dur_nsec),
-            float(tot_dur), float(flows), float(pktperflow), float(byteperflow),
-             float(pktrate), float(port_no), float(tx_bytes),
-       float(rx_bytes), float(tx_kbps), float(randint(0,1)), float(randint(0,1)), float(randint(0,1)), float(randint(0,1))])
-        
+          [float(pktcount),float(bytecount),float(dur), float(dur_nsec),float(tot_dur),
+           float(flows), float(pktperflow), float(byteperflow),float(pktrate),
+           float(port_no), float(tx_bytes),float(rx_bytes), float(tx_kbps), 
+           float(randint(0,1)), float(randint(0,1)), float(randint(0,1)), float(randint(0,1))])
         
     st.success(network)
-    
-    
-    
-    
+
 if __name__ == '__main__':
     main()
